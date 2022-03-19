f92711cf0c4a84fbbf963af2d4cad4b7b5460ed2
Suleyman Musayev <slmusayev@gmail.com>
2022-03-19 23:15:53 +0500

    change to db seeds to set up initial products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.delete_all
+#
+Product.create!(title:'Programming Ruby 1.9 & 2.0',
+description:
+%{<p>
+    Ruby is the fastest growing and most exciting dynamic language 
+    out there.
+</p>},
+image_url: 'ruby.jpg',
+price: 49.99)
+#
