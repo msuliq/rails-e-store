8d06411ef7dcb93ad83fe7eb66b497b6938f22a7
Suleyman Musayev <slmusayev@gmail.com>
2022-03-22 11:05:42 +0500

    update application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :carts
+  get 'store/index'
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'store#index', as: 'store'
 end
