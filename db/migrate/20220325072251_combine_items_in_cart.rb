37199f0bf7eeee349f9575e48e39c2a64e8f227d
Suleyman Musayev <slmusayev@gmail.com>
2022-03-25 12:30:43 +0500

    update migrate db for quantity grouping in cart

--- db/migrate/20220325072251_combine_items_in_cart.rb
+++ db/migrate/20220325072251_combine_items_in_cart.rb
@@ -18,4 +18,17 @@ class CombineItemsInCart < ActiveRecord::Migration[7.0]
       end
     end
   end
+
+  def down
+    # separate records with quantity>1 to several records
+    LineItem.where("quantity>1").each do |line_item|
+      # add individual items
+      line_item.quantity.times do
+        LineItem.create cart_id: line_item.cart_id,
+        product_id: line_item.product_id, quantity: 1
+      end
+    # delete the initial record
+    line_item.destroy
+    end
+  end
 end
