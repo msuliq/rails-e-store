14867d8451b9bd062caf34048890b5292f5b363c
Suleyman Musayev <slmusayev@gmail.com>
2022-03-29 13:53:42 +0500

    add mailers for order confirmation and shipping

--- app/mailers/order_notifier_mailer.rb
+++ app/mailers/order_notifier_mailer.rb
@@ -1,14 +1,15 @@
 class OrderNotifierMailer < ApplicationMailer
 
+  default from: 'Sam Ruby <rails@example.com>'
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_notifier_mailer.received.subject
   #
-  def received
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'Confirmation for your order at Rails E-Store'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -16,9 +17,9 @@ class OrderNotifierMailer < ApplicationMailer
   #
   #   en.order_notifier_mailer.shipped.subject
   #
-  def shipped
-    @greeting = "Hi"
+  def shipped(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Your order from Rails E-Store is on the way'
   end
 end
